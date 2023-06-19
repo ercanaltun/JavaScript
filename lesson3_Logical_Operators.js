1966f618a9367edbe8a0de2c578939c16ffb42ca
Ercan ALTUN <salsel0147@gmail.com>
2023-06-19 19:15:53 +0300

    20.06.2023

--- lesson3_Logical_Operators.js
+++ lesson3_Logical_Operators.js
@@ -1,4 +1,14 @@
 var num1 =5;
 var num2 = 9;
-console.log(num1==5);
-console.log(num1=="5");
+
+console.log(num1==5);      //true döner 
+console.log(num1=="5");   // true döner      " == eşittir işareti sadece verinin eşit olup olmadığını kontrol eder."
+
+
+console.log(num2===9);    //true döner
+console.log(num2==="9");  //false döner     " === eşittir işareti hem verinin hem de veri tipinin eşit olup olmadığını kontrol eder."
+
+
+console.log(num1>4);       //true döner
+console.log(num1>"4");    // true döner      " > , < , >= , <= , != işaretleri sadece verinin eşit olup olmadığını kontrol eder."
+
